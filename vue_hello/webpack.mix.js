0f89517908f7c7622e705acbdb86b32c7740f31b
yuki-snow1823 <magic.iroha@gmail.com>
2020-12-20 14:25:36 +0900

    add: mixの変更

--- vue_hello/webpack.mix.js
+++ vue_hello/webpack.mix.js
@@ -1,3 +1,12 @@
 let mix = require("laravel-mix");
 
-mix.js("resources/js/app.js", "public/js");
+mix
+  .js("resources/js/home.js", "public/js")
+  .sass("resources/sass/app.scss", "public/css", {
+    implementation: require("node-sass"),
+  });
+
+mix.browserSync({
+  files: ["public/**/*.*"],
+  proxy: "http://localhost:8000",
+});
